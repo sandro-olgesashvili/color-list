eb273204c916152978397d114968f6e8c1f2c313
sandro-olgesashvili <>
2024-12-13 18:11:08 +0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,73 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TextFormatPipe } from './text-format.pipe';
+import { BackgroundColorEffectDirective } from './background-color-effect.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TextFormatPipe,
+    BackgroundColorEffectDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'color-list';
+  colors: string[] = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'orange',
+    'purple',
+    'pink',
+    'brown',
+    'gray',
+    'black',
+    'white',
+    'violet',
+    'indigo',
+    'cyan',
+    'magenta',
+    'teal',
+    'turquoise',
+    'lime',
+    'gold',
+    'silver',
+    'beige',
+    'coral',
+    'peach',
+    'maroon',
+    'navy',
+    'olive',
+    'plum',
+    'salmon',
+    'sienna',
+    'orchid',
+    'crimson',
+    'lavender',
+    'mint',
+    'fuchsia',
+    'chartreuse',
+    'azure',
+    'emerald',
+    'ruby',
+  ];
+  colorList: string[] = [];
+  colorInput: string = '';
+
+  onAddColor() {
+    this.colorInput = this.colorInput.trim();
+    this.colorInput = this.colorInput.toLowerCase();
+    let colorExists = this.colors.find((x) => x === this.colorInput);
+    if (!colorExists) return alert('wrong color name');
+    let checkColor = this.colorList.find((x) => x === this.colorInput);
+    if (checkColor) return alert('Color already exists!');
+
+    this.colorList.push(this.colorInput);
+    this.colorInput = '';
+  }
 }
